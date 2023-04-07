cf5ccd09b5461f6af91b3eb4ba86a04d3ab25697
Zach Padovano <zacharyaz10@gmail.com>
2023-04-07 11:23:10 -0700

    'FirstCommit'

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string deviceName = "Xbox";
+            int howOld = 2;
+            char excited = '!';
+            bool valid = true;
+            double speed = 150;
+            decimal badSpeed = 5.5m;
+            Console.WriteLine($"I have a {deviceName} and I've had it for {howOld} years now{excited}. Its {valid}, sometimes it can lag but thats ONLY because my wifi gets like {speed} mbps and sometimes drops to like {badSpeed}.");
         }
     }
 }
